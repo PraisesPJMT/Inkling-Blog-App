d749327ec8ae45e6db8db39c0a2a64dafa91d4a8
PraisesPJMT <praisesmusa@outlook.com>
2022-11-30 13:40:54 +0100

    Add `Like` model class

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,9 @@
 class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_save :update_likes_counter
+  def update_likes_counter
+    post.update(likes_counter: post.likes.size)
+  end
 end
