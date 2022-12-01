b462f82036c43468525bc98339c199334123a200
PraisesPJMT <praisesmusa@outlook.com>
2022-12-01 16:44:22 +0100

    Add validation for `User` model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
   def recent_posts
     posts.order('created_at Desc').limit(3)
   end
