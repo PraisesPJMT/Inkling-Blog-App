172006ef4f1aee6aa12e92332fdaedf02543e941
PraisesPJMT <praisesmusa@outlook.com>
2022-12-06 13:57:02 +0100

    Add tests for `User` controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,40 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+    # user_path => /users/index
+
     it 'returns http success' do
-      get '/users/index'
+      get '/users/'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'render right template' do
+      get '/users/'
+      expect(response).to render_template(:index)
+    end
+
+    it 'render includes the title "Inkling Blog Users"' do
+      get '/users/'
+      expect(response.body).to include('Inkling Blog Users')
     end
   end
 
   describe 'GET /show' do
+    # user_path(:user_id) => /users/:user_id
+
     it 'returns http success' do
-      get '/users/show'
+      get '/users/1'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'render right template' do
+      get '/users/1'
+      expect(response).to render_template(:show)
+    end
+
+    it 'render includes the sub-title "Bio"' do
+      get '/users/1'
+      expect(response.body).to include('Bio')
     end
   end
 end
