121ceac27a8413731d1a9a94692a7c6341b066da
PraisesPJMT <praisesmusa@outlook.com>
2022-12-09 17:37:23 +0100

    Add new `Posts`, `Comments`, and `Likes` paths to route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   root to: "users#index"
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+      resources :likes, only: %i[create]
+    end
   end
 end
