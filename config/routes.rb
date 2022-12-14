38c9f22d042423995a5a456d12a6b056a7a4d55b
PraisesPJMT <praisesmusa@outlook.com>
2022-12-14 17:17:57 +0100

    Add `device` route to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # devise_scope :user do
+  #   root to: "users#index"
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
