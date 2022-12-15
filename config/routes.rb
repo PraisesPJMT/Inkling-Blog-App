cf21fd73c39c43539dfd3cab77a2236fc70ea6c6
PraisesPJMT <praisesmusa@outlook.com>
2022-12-15 17:44:54 +0100

    Add routes for destroying posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   root to: "users#index"
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[new create]
+    resources :posts, only: %i[index show new create destroy] do
+      resources :comments, only: %i[new create destroy]
       resources :likes, only: %i[create]
     end
   end
